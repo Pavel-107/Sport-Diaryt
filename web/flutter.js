95231e1d1d183a539de21240bb3649b079a5f1fb
Pavel-107 <Pavel-107@mail.ru>
2025-10-18 22:08:46 +1100

    Create flutter.js

---
+++ web/flutter.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+  _flutter.loader.loadEntrypoint({
+    serviceWorker: { serviceWorkerVersion: null },
+    onEntrypointLoaded: function(engineInitializer) {
+      engineInitializer.initializeEngine().then(function(appRunner) {
+        appRunner.runApp();
+      });
+    }
+  });
+});
