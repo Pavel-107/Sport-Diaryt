7ff746049c146194b298faaed663a37c4e745e85
Pavel-107 <Pavel-107@mail.ru>
2025-11-01 08:18:43 +1100

    Update install_flutter.sh

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
+
 # Install Flutter SDK
 git clone https://github.com/flutter/flutter.git -b stable
+
+# Add Flutter to PATH for this script
 export PATH="$PATH:`pwd`/flutter/bin"
-source ~/.bashrc
-flutter doctor
+
+# Verify installation
+flutter doctor -v
